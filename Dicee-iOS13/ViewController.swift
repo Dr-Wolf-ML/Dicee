bd39990a47c5f06179aa2779f4d3ef9ac63b7b62
Dr-Wolf-ML <wolfgangml@mac.com>
2020-08-21 15:05:45 +1000

    Counting up & down

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    var allTheDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    var countUp = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = allTheDice[leftDiceNumber]
+        diceImageView2.image = allTheDice[rightDiceNumber]
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        if (leftDiceNumber == 0) {
+            countUp = true
+        } else if (leftDiceNumber == 5) {
+            countUp = false
+        }
+        
+        switch countUp {
+        case true:
+            leftDiceNumber += 1
+            rightDiceNumber -= 1
+        default:
+            leftDiceNumber -= 1
+            rightDiceNumber += 1
+        }
+        
+        diceImageView1.image = allTheDice[leftDiceNumber]
+        diceImageView2.image = allTheDice[rightDiceNumber]
+    }
+    
 }
 
